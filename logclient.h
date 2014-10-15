47d503e663bbfa23520ef7973088fbcd959705de
tujiaw <tujiaw@163.com>
2014-10-15 14:04:45 +0800

    Update logclient.h

--- logclient.h
+++ logclient.h
@@ -19,3 +19,51 @@ private:
 #define MYLOG LogClient(__FILE__, __LINE__).stream()
 
 #endif // LOGCLIENT_H
+
+
+//#pragma once
+//#include <QTextStream>
+//
+//#define CLOG ConsoleLog(__FILE__, __LINE__).stream()
+//#define ULOG UdpLog(__FILE__, __LINE__).stream()
+//#define FLOG FileLog(__FILE__, __LINE__).stream()
+//
+//class RtLog
+//{
+//public:
+//	RtLog(const QString &file, quint32 line);
+//	virtual ~RtLog() = 0;
+//	QTextStream& stream() { return stream_; }
+//
+//protected:
+//	QString buffer_;
+//	QTextStream stream_;
+//};
+//
+//class ConsoleLog : public RtLog
+//{
+//public:
+//	ConsoleLog(const QString &file, quint32 line);
+//	~ConsoleLog();
+//};
+//
+//class QUdpSocket;
+//class UdpLog : public RtLog
+//{
+//public:
+//	UdpLog(const QString &file, quint32 line);
+//	~UdpLog();
+//
+//private:
+//	static QUdpSocket *s_udp;
+//};
+//
+//class FileLog : public RtLog
+//{
+//public:
+//	FileLog(const QString &file, quint32 line);
+//	~FileLog();
+//
+//private:
+//	static QTextStream *writeStream_;
+//};
