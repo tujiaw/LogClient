98feadab3ead3ef98fd7ebac6627e1bcf669dc1b
tujiaw <tujiaw@163.com>
2014-10-15 14:05:14 +0800

    Update logclient.cpp

--- logclient.cpp
+++ logclient.cpp
@@ -23,3 +23,68 @@ LogClient::~LogClient()
 }
 
 
+//#include "RtLog.h"
+//#include <QDateTime>
+//#include <QtNetwork/QUdpSocket>
+//#include <QFile>
+//#include <QDebug>
+//
+//RtLog::RtLog(const QString &file, quint32 line)
+//	: stream_(&buffer_, QIODevice::Append)
+//{
+//	stream_ << "[" << QDateTime::currentDateTime().toString("HH:mm:ss.zzz") << "," << file.section("\\", -1) << "," << line << "] ";
+//}
+//
+//RtLog::~RtLog()
+//{
+//}
+//
+////---------------------------------------------------------
+//ConsoleLog::ConsoleLog(const QString &file, quint32 line)
+//	: RtLog(file, line)
+//{
+//}
+//
+//ConsoleLog::~ConsoleLog()
+//{
+//	qDebug() << buffer_;
+//}
+//
+////---------------------------------------------------------
+//QUdpSocket* UdpLog::s_udp = NULL;
+//UdpLog::UdpLog(const QString &file, quint32 line)
+//	: RtLog(file, line)
+//{
+//	if (NULL == s_udp) {
+//		s_udp = new QUdpSocket();
+//	}
+//}
+//
+//UdpLog::~UdpLog()
+//{
+//	if (s_udp) {
+//		QByteArray aArray = buffer_.toUtf8();
+//		s_udp->writeDatagram(aArray.data(), aArray.size(), QHostAddress("127.0.0.1"), 5566);
+//	}
+//}
+//
+////---------------------------------------------------------------------
+//QTextStream* FileLog::writeStream_ = NULL;
+//FileLog::FileLog(const QString &file, quint32 line)
+//	: RtLog(file, line)
+//{
+//	if (NULL == writeStream_) {
+//		QFile *data = new QFile("output.txt");
+//		if (data->open(QIODevice::Append)) {
+//			writeStream_ = new QTextStream(data);
+//		}
+//	}
+//}
+//
+//FileLog::~FileLog()
+//{
+//	if (writeStream_) {
+//		*writeStream_ << buffer_ << "\n";
+//		writeStream_->flush();
+//	}
+//}
